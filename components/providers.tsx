c0d084b4c8dee76f0d8f3c6246d04255c0487e30
Michael Sulistio <michaelsulistio@gmail.com>
2025-08-14 23:39:40 -0600

    Add MWA

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,7 +1,25 @@
 "use client";
-
+import { 
+    createDefaultAuthorizationCache, 
+    createDefaultChainSelector, 
+    createDefaultWalletNotFoundHandler,
+    registerMwa, 
+} from '@solana-mobile/wallet-standard-mobile';
 import { PrivyProvider } from "@privy-io/react-auth";
 import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+
+registerMwa({
+    appIdentity: {
+      name: 'My app',
+      uri: 'https://solanamobile.com',
+      icon: 'favicon.ico', // resolves to https://myapp.io/relative/path/to/icon.png
+    },    
+    authorizationCache: createDefaultAuthorizationCache(),
+    chains: ['solana:devnet', 'solana:mainnet'],
+    chainSelector: createDefaultChainSelector(),
+    onWalletNotFound: createDefaultWalletNotFoundHandler(),
+    // remoteHostAuthority: '<REPLACE_WITH_URL_>', // Include to enable remote connection option.
+})
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
